e3dece573e46d16c57d5d22311505a449c070de1
Ally Hangartner <>
2018-07-01 17:10:23 -0400

    fixed issues identified with linter

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 console.log("Welcome to Ally's portfolio site")
 
-$(document).ready(function() {
-  $(".project-preview").on("click", function() {
-    $(this).siblings(".project-details").slideToggle();
-    });
-});
+$(document).ready(function () {
+  $('.project-preview').on('click', function () {
+    $(this).siblings('.project-details').slideToggle()
+  })
+})
 
-$(".preview-img").on("click", function() {
-    var element = document.getElementsByTagName(".preview-img");
-    element.classList.toggle(".grayscale");
-});
+$(document).ready(function() {
+  $('.project-preview').on('click', function () {
+    $(this).siblings('.preview-img').slideToggle('.grayscale')
+  })
+})
